ee529148a6bf9c461f583f6cdca134ffc4f9b800
Michael Agboola <magboola@terragonltd.com>
2020-05-02 14:10:52 +0100

    Add test for Weather

---
+++ src/weather/weather.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+
+
+const getWeather = async ( latitude, longitude ) => {
+
+    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${ latitude }&lon=${ longitude }&appid=32f843d833c38373032f825c4a92418a&units=metric`)
+
+    return resp.data.main.temp;
+
+}
+
+
+module.exports = {
+    getWeather
+}
--- test.js
+++ test.js
@@ -1,17 +1,33 @@
-const place = require('./src/location/location');
+const place = require("./src/location/location");
+const weather = require("./src/weather/weather");
 const expect = require("chai").expect;
 
 describe("invisible technologies assessment test cases", () => {
   describe("Coordinate test", () => {
     context("get longitude and latitude for a given location", () => {
-      it("should return an object with key: address", async() => {
-        expect(await place.getPlaceLatLng('10005')).to.be.an("object").and.haveOwnProperty("address");
+      it("should return an object with key: address", async () => {
+        expect(await place.getPlaceLatLng("10005"))
+          .to.be.an("object")
+          .and.haveOwnProperty("address");
       });
-      it("should return an object with key: lat", async() => {
-        expect(await place.getPlaceLatLng('New York')).to.be.an("object").and.haveOwnProperty("lat");
+      it("should return an object with key: lat", async () => {
+        expect(await place.getPlaceLatLng("Tokyo"))
+          .to.be.an("object")
+          .and.haveOwnProperty("lat");
       });
-      it("should return an object with key: long", async() => {
-        expect(await place.getPlaceLatLng('Japan')).to.be.an("object").and.haveOwnProperty("lng");
+      it("should return an object with key: long", async () => {
+        expect(await place.getPlaceLatLng("São Paulo"))
+          .to.be.an("object")
+          .and.haveOwnProperty("lng");
+      });
+    });
+  });
+  describe("Coordinate test", () => {
+    context("get weather for a given set of coordinate", () => {
+      it("should return an a number ", async () => {
+        expect(await weather.getWeather(40.7064363, -74.0094562)).to.be.an(
+          "number"
+        );
       });
     });
   });
